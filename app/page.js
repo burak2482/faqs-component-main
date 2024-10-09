3c0397599e6dd225088926a18542edf742281f02
Burak Durmuşoğlu <108025275+burak2482@users.noreply.github.com>
2024-10-09 19:10:38 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,10 +6,10 @@ import Accordion from './components/Accordion';
 export default function Home() {
   return (
     <main className='background-pattern flex flex-col justify-center items-center min-h-screen bg-customPink'>
-      <Image src='/background-pattern-desktop.svg' class='' alt='' width={1} height={1}/>
+      <Image src='./background-pattern-desktop.svg' class='' alt='' width={1} height={1}/>
       <section class='w-1/4 bg-white px-10 py-16 rounded-2xl'>
         <div class='flex items-center'>
-          <Image src='/icon-star.svg' class='' alt='' width={50} height={50}/>
+          <Image src='./icon-star.svg' class='' alt='' width={50} height={50}/>
           <h1 class='font-customNormal font-black text-5xl ml-4'>FAQs</h1>
         </div>
         <div class='flex mt-5'>
